50e6d9ab1f5f7df9edac352df48da508a3ba99b5
Anna Abbott <annaabbottdesign@gmail.com>
2023-11-23 19:53:23 -0600

    added search endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors"; 
+import cors from "cors";
 
 import * as RecipeAPI from "./recipe-api";
 
@@ -9,10 +9,12 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/api/recipe/search", async (req, res) => {
-    res.json({ message: "success" });
-  });
+  const searchTerm = req.query.searchTerm as string;
+  const page = parseInt(req.query.page as string);
+  const results = await RecipeAPI.searchRecipes(searchTerm, page);
+  return res.json(results);
+});
 
-  app.listen(5000, () => {
-    console.log("Server running on localhost:5000");
-  });
-  
+app.listen(5000, () => {
+  console.log("Server running on localhost:5000");
+});
